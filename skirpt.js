f1cd0434ba60a8b3c6bc3a41452e04d8803d5ed8
SPI339 <uczniowie339@gmail.com>
2024-07-12 13:07:01 +0200

    Add files via upload

---
+++ skirpt.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0.0000001,
+    });
+  });
+              
